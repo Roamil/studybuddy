a2fe2515e5eaacaff4be47e489293ec84acd0a05
roamil <roamilkachchhi@gmail.com>
2023-04-24 14:39:26 +0500

    commit

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
@@ -20,3 +21,5 @@ urlpatterns = [
     path('topics/', views.topicsPage, name="topics"),
     path('activity/', views.activityPage, name="activity"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
